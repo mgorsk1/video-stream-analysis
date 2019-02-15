80b8a9004b35944a7dc0f374023c05fad114c614
mgorski <mariusz.gorski@onwelo.com>
2019-02-15 21:40:12 +0100

    feat(result db): elasticsearch init

--- app/database.py
+++ app/database.py
@@ -1,6 +1,7 @@
 from redis import Redis
 from json import dumps
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, ElasticsearchException
+from datetime import datetime
 
 from config import log
 
@@ -41,9 +42,50 @@ class TemporaryDatabase:
 
 
 class ResultDatabase:
-    def __init__(self):
-        self.db = Elasticsearch()
+    def __init__(self, host, port):
+        self.db = Elasticsearch(hosts=[dict(host=host, port=port)])
 
     def check_if_exists(self, field, value, fuzzy):
-        fuzzy_query = dict()
-        pass
+        if fuzzy:
+            query = {
+                "query": {
+                    "match": {
+                        field: {
+                            "query": value,
+                            "fuzziness": 1
+                        }
+                    }
+                }
+            }
+        else:
+            query = {
+                "query": {
+                    "match": {
+                        field: value
+                    }
+                }
+            }
+
+        try:
+            search = self.db.search('plates', 'default', query)
+        except ElasticsearchException:
+            log.error("#error querying #elasticsearch", exc_info=True)
+            return False
+
+        results = search.get('hits').get('hits')
+
+        log.debug("#results for field #received", extra=dict(field=field,
+                                                             value=value,
+                                                             results=results))
+        if len(results) > 0:
+            return True
+        else:
+            return False
+
+    def index_result(self, plate, confidence, id, **kwargs):
+        body = dict(plate=plate, confidence=confidence, fileName="{}_{}.png".format(plate, id))
+        body['@timestamp'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')
+
+        body.update(dict(kwargs))
+
+        self.db.index('plates', 'default', body, id=id)
