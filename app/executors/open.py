7f1c08405b2dc8cf00aab9c0196c7258e0c67acb
mrp1nk <gorskimariusz13@gmail.com>
2019-11-06 21:01:09 +0100

    :recycle: base opener class

--- app/executors/open.py
+++ app/executors/open.py
@@ -1,18 +1,19 @@
 from json import dumps
 from time import time, sleep
+from abc import abstractmethod, ABC
 
 from app.config import log
 from app.executors.base import BaseExecutor
 
 
-class GateOpener(BaseExecutor):
+class BaseOpener(ABC, BaseExecutor):
     def __init__(self, *args, **kwargs):
-        super(GateOpener, self).__init__(*args, **kwargs)
+        super(BaseOpener, self).__init__(*args, **kwargs)
 
-        self.open_time = 120
+        self.open_time = kwargs.get('open_time', 120)
 
     def _action(self, value, confidence, image, uuid, **kwargs):
-        super(GateOpener, self)._action(value, confidence, image, uuid)
+        super(BaseOpener, self)._action(value, confidence, image, uuid)
 
         self._open_gate(value)
 
@@ -20,22 +21,42 @@ class GateOpener(BaseExecutor):
 
         self._close_gate(value)
 
-    def _open_gate(self, value):
+    def open(self, value):
         data = dict(timestamp=time(), value=value)
 
-        self.tdb.set_val('gate:open', dumps(data))
+        self.tdb.set_val(f'#{self.o} open', dumps(data))
 
+        self._open(value)
+
+        log.info(f"#{self.o} opened", extra=dict(value=value))
+
+    def close(self, value):
+        data = dict(timestamp=time(), value=value)
+
+        self.tdb.del_val(f'#{self}:open')
+
+        self._close(value)
+
+        self.tdb.set_val(f'#{self}:close', dumps(data))
+
+        log.info(f'#{self} closed', extra=dict(value=value))
+
+    @abstractmethod
+    def _open(self):
+        pass
+
+    @abstractmethod
+    def _close(self):
+        pass
+
+
+class GateOpener(BaseExecutor):
+    o = 'gate'
+
+    def _open(self, value):
         # @todo below actually trigger opening gate
-
-        log.info("#gate opened for #vehicle", extra=dict(value=value))
+        pass
 
     def _close_gate(self, value):
-        data = dict(timestamp=time(), value=value)
-
-        self.tdb.del_val('gate:open')
-
         # @todo below actually trigger closing gate
-
-        self.tdb.set_val('gate:close', dumps(data))
-
-        log.info("#gate closed", extra=dict(value=value))
+        pass
